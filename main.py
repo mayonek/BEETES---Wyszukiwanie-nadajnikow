4591709a38cf3b709f0977ef2e58f044f6cc088f
Ryzen 2700x <mayonjobs@gmail.com>
2020-02-25 00:46:47 +0100

    Zmiana Całego kodu - Dzielenie nadajnikow - Tryby sieci - Gctr
    + MAIN test

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from math import sin, cos, asin, atan2, sqrt, pi
 import json
 from geopy.geocoders import Nominatim
 import time
+
 geolocator = Nominatim(user_agent="my-application")
 import substring
+
 loadbarwidth = 10
 transmitters = []
 for i in range(1, loadbarwidth + 1):
@@ -12,11 +14,12 @@ for i in range(1, loadbarwidth + 1):
     strbarwidth = '[{}{}] - {}\r'.format(
         (i * '✓'),
         ((loadbarwidth - i) * '-'),
-        (('{:0.2f}'.format(((i) * (100/loadbarwidth))) + '% Lączenie z Mapami'))
+        (('{:0.2f}'.format(((i) * (100 / loadbarwidth))) + '% Lączenie'))
     )
 
-    print(strbarwidth ,end = '')
-print("BE TE ES v2 Piotr.Wawrzyczek@netia.pl")
+    print(strbarwidth, end='')
+print("BE TE ES v2 Piotr.Wawrzyczek@netia.pl ")
+
 
 # def calculate_distance(lat1, lon1, lat2, lon2):
 #   return geopy.distance.geodesic((lat1, lon1), (lat2, lon2)).km
@@ -25,11 +28,11 @@ print("BE TE ES v2 Piotr.Wawrzyczek@netia.pl")
 # def calculate_distance(lat1, lon1, lat2, lon2):
 #   return geopy.distance.vincenty((lat1, lon1), (lat2, lon2)).km
 
-#wzór najdokładniejszy
+# wzór najdokładniejszy
 def calculate_distance(lat1, lon1, lat2, lon2):
-  p = 0.017453292519943295     #Pi/180
-  a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-  return 12742 * asin(sqrt(a)) #2*R*asin...
+    p = 0.017453292519943295  # Pi/180
+    a = 0.5 - cos((lat2 - lat1) * p) / 2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    return 12742 * asin(sqrt(a))  # 2*R*asin...
 
 
 # def rad(x):
@@ -47,105 +50,107 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 
 # podział bazy
 class Transmitter:
-  def __init__(self, address, distance, _id, network_modes):
-    self.address = address
-    self.distance = distance
-    self._id = _id
-    self.network_modes = network_modes
-  #PRINTOWANIE WYNIKÓW
-  def print(self):
-    print('<-')
-    print('Położenie BT: %s' % self.address)
-    print('Dystans: %.2fkm' % self.distance)
-    print('BT: %s' % self._id)
-    print('Tryby sieci: %s' % ', '.join(self.network_modes))
-    #print('->')
-    #SZUKANIE GCTR
-    gctropen = open('gctr.xls', mode='r+')
-    gctrread = gctropen.readlines()
-    warun = 0
-    liniaaa = 0
-    bts = substring.substringByInd(self._id, startInd=2, endInd=5)
-    for line in gctrread:
-      if  bts in line:
-        warun = warun + 1
-        liniaaa = line
-        gctr = substring.substringByChar(liniaaa, startChar='>', endChar=".")
-        gctr1 = gctr[31:]
-    if warun == 0:
-        print("Brak Aktywnego GCTR na "+self._id )
+    def __init__(self, address, distance, _id, network_modes):
+        self.address = address
+        self.distance = distance
+        self._id = _id
+        self.network_modes = network_modes
 
-    else:
-        print("Znaleziono Aktywny GCTR : " + (gctr1))
-    return
-  def to_dictionary(self):
-    return {'address': self.address, 'distance': self.distance, 'id': self._id, 'network_modes': self.network_modes}
+    # PRINTOWANIE WYNIKÓW
+    def print(self):
+        print('✓')
+        print('Położenie BT: %s' % self.address)
+        print('Dystans: %.2fkm' % self.distance)
+        print('BT: %s' % self._id)
+        print('Tryby sieci: %s' % ', '.join(self.network_modes))
+        # print('->')
+        # SZUKANIE GCTR
+        gctropen = open('gctr.xls', mode='r+')
+        gctrread = gctropen.readlines()
+        warun = 0
+        liniaaa = 0
+        bts = substring.substringByInd(self._id, startInd=2, endInd=5)
+        for line in gctrread:
+            if bts in line:
+                warun = warun + 1
+                liniaaa = line
+                gctr = substring.substringByChar(liniaaa, startChar='>', endChar=".")
+                gctr1 = gctr[31:]
+        if warun == 0:
+            print("Brak Aktywnego GCTR na " + self._id)
+
+        else:
+            print("Znaleziono Aktywny GCTR : " + (gctr1))
+
+    def to_dictionary(self):
+        return {'address': self.address, 'distance': self.distance, 'id': self._id, 'network_modes': self.network_modes}
 
 
 def save_to_file(n):
-  save_path = 'wyniki/{}_lat_{}_lon_{}.json'.format(time.strftime("%Y-%m-%d_%Hx%M"), lat1, lon1)
-  try:
-    with open(save_path, 'w+') as f:
-      f.write(json.dumps([t.to_dictionary() for t in transmitters[:n]], ensure_ascii=False, indent=4))
-    print('Saved as %s' % save_path)
-  except:
-    print('Couldn\'t save the output')
-def start():
-  while True:
-    print("Wprowadź adress:")
-    city = input()
-    country = "PL"
+    save_path = 'wynik/{}_lat_{}_lon_{}.json'.format(time.strftime("%Y-%m-%d_%Hx%M"), lat1, lon1)
     try:
-      loc = geolocator.geocode(city + ',' + country)
-    except Exception as e:
-      break
-    if not loc:
-      print("!!podałes bledne współrzędne!!!")
-      continue
+        with open(save_path, 'w+') as f:
+            f.write(json.dumps([t.to_dictionary() for t in transmitters[:n]], ensure_ascii=False, indent=4))
+        print('Saved as %s' % save_path)
+    except:
+        print('Couldn\'t save the output')
 
-    print("Generowanie linku..")
-    print()
-    x = str(loc.latitude)
-    y = str(loc.longitude)
 
-    wspol = (x + "," + y)
-    url = 'http://beta.btsearch.pl/?dataSource=locations&network=26001&standards=&bands=&center=' + (wspol) + '&zoom=20'
-    print(url)
+def start():
+    while True:
+        print("Wprowadź adres:")
+        city = input()
+        country = "PL"
+        try:
+            loc = geolocator.geocode(city + ',' + country)
+        except Exception as e:
+            break
+        if not loc:
+            print("!!podałes bledne współrzędne!!!")
+            continue
 
-    global lat1
-    lat1 = loc.latitude
-    global lon1
-    lon1 = loc.longitude
-    break
+        print("Generowanie linku..")
+        print()
+        x = str(loc.latitude)
+        y = str(loc.longitude)
+
+        wspol = (x + "," + y)
+        url = 'http://beta.btsearch.pl/?dataSource=locations&network=26001&standards=&bands=&center=' + (
+            wspol) + '&zoom=20'
+        print(url)
+
+        global lat1
+        lat1 = loc.latitude
+        global lon1
+        lon1 = loc.longitude
+        break
+
+
 def init():
-  with open('distance.json') as f:
-    data = json.load(f)
-  for e in data:
-    distance = calculate_distance(lat1, lon1, float(e['lat']), float(e['lon']))
-    transmitters.append(Transmitter(e['address'], distance, e['id'], e['network_modes']))
-  transmitters.sort(key=lambda x: x.distance)
+    with open('distance.json', encoding="windows-1250") as f:
+        data = json.load(f)
+    for e in data:
+        distance = calculate_distance(lat1, lon1, float(e['lat']), float(e['lon']))
+        transmitters.append(Transmitter(e['address'], distance, e['id'], e['network_modes']))
+    transmitters.sort(key=lambda x: x.distance)
 
 
 def loop():
-  # while True:
+    # while True:
     # n = int(input('How many results? '))
-  n = 3
+    n = 3
 
-  save_to_file(n)
-  for t in transmitters[:n]:
-    t.print()
-  main()
-
-
-
-
+    save_to_file(n)
+    for t in transmitters[:n]:
+        t.print()
+    main()
 
 
 def main():
-  start()
-  init()
-  loop()
+    start()
+    init()
+    loop()
 
 
 if __name__ == '__main__':
-  main()
+    main()
